f5325c155c0780dd1cd3303ad0899c913ac36ffa
seftyaaaaa <126318039+seftyaaaaa@users.noreply.github.com>
2023-05-10 09:33:12 +0700

    Update and rename tes.c to perpus.c
    edit fungsi main

---
+++ perpus.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+    int pilihan;
+    printf("====Selamat datang di Perpustakaan Statistika====n:\n");
+    printf("1. Tambah buku\n");
+    printf("2. Cari buku\n");
+    printf("3. Pinjam buku\n");
+    printf("4. Kembalikan buku\n");
+    printf("5. Keluar\n");
+    printf("Pilihan anda: ");
+    scanf("%d", &pilihan);    
+}
